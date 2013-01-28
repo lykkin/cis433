3c856f0125b956b6d9dfc7ae63001ca4edd409ca
CIS 415 Student <cis415@cis415-xubuntu1204-vbox.(none)>
2013-01-27 23:06:59 -0800

    it encrypts! still needs comments and shit

--- assn1/uoenc.c
+++ assn1/uoenc.c
@@ -6,30 +6,30 @@
 int main(int argc, char *argv[])
 {
 
-//initialization taken from the man pages
-/* Version check should be the very first call because it
-makes sure that important subsystems are intialized. */
-if (!gcry_check_version (GCRYPT_VERSION))
-{
-fputs ("libgcrypt version mismatch\n", stderr);
-exit (2);
-}
+  //initialization taken from the man pages
+  /* Version check should be the very first call because it
+  makes sure that important subsystems are intialized. */
+  if (!gcry_check_version (GCRYPT_VERSION))
+  {
+    fputs ("libgcrypt version mismatch\n", stderr);
+    exit (2);
+  }
 
-/* Disable secure memory.  */
-gcry_control (GCRYCTL_DISABLE_SECMEM, 0);
-  
-/* ... If required, other initialization goes here.  */
-/* Tell Libgcrypt that initialization has completed. */
-gcry_control (GCRYCTL_INITIALIZATION_FINISHED, 0);
+  /* Disable secure memory.  */
+  gcry_control (GCRYCTL_DISABLE_SECMEM, 0);
+    
+  /* ... If required, other initialization goes here.  */
+  /* Tell Libgcrypt that initialization has completed. */
+  gcry_control (GCRYCTL_INITIALIZATION_FINISHED, 0);
 
-int local = 1;
-  char address[16];
+  int local = 1;
+  char address[17];
   int port;
   char fileName[100];
   int fileSpecified = 0;
-  char salt[16];
-  char password[32];
-  char key[32];
+  char salt[17];
+  char password[33];
+  char key[33];
   
   if(argc > 4){
     printf("Incorrect syntax, should be: ./uoenc <input file> [-d <output IP-addr:port>] [-l]");
@@ -74,23 +74,40 @@ int local = 1;
     strcat(encFileName, ".uo");
     FILE * encFile = fopen(encFileName, "a+");
     if(fgetc(encFile) != EOF){
-      printf("Output file already exists, exitting.\n");
+      printf("%s already exists, exitting.\n", encFileName);
       fclose(srcFile);
       fclose(encFile);
       exit(1);
     }
     //Get a password from the user
     printf("Password: ");
-    for(i = 0; i < 32; i++){
+    for(i = 0; i < 33; i++){
       password[i] = '\0';
     }
     fgets(password, 32, stdin);
     //generate a salt and key
     gcry_randomize(salt, 16, GCRY_STRONG_RANDOM);
-    salt[15] = '\0';
+    salt[16] = '\0';
     gpg_error_t err = gcry_kdf_derive(password, strlen(password), GCRY_KDF_PBKDF2, GCRY_MD_SHA256, salt, strlen(salt), 100, 32, key);
-    key[31] = '\0';
-    
+    key[32] = '\0';
+    fputs(salt, encFile);
+    gcry_cipher_hd_t cipher;
+    err = gcry_cipher_open(&cipher, GCRY_CIPHER_AES256, GCRY_CIPHER_MODE_CBC, 0);
+    err = gcry_cipher_setkey(cipher, key, strlen(key));
+    char nonce[33];
+    gcry_create_nonce(nonce, 32);
+    nonce[32] = '\0';
+    err = gcry_cipher_setiv(cipher, nonce, strlen(nonce));
+    char curBlock[1025];
+    while(!feof(srcFile)){
+      fread(curBlock, 1, 1024, srcFile);
+      curBlock[1024] = '\0';
+      printf("%s\n%d\n",curBlock, strlen(curBlock));
+      gcry_cipher_encrypt(cipher, curBlock, 1024, curBlock, 1024);
+      printf("%s\n%d\n",curBlock, strlen(curBlock));
+      fwrite(curBlock, 1, 1024, encFile);
+    }
+
     if(local){
     
     } else {
